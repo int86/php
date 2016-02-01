0fa5d206e515ead2302d294a37b683407d67a00b
int86 <273206894@qq.com>
2016-02-01 11:30:39 +0800

    complete curl

--- movie.php
+++ movie.php
@@ -106,6 +106,7 @@ function getHtml($url){
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $html = curl_exec($ch);
     curl_close($ch);
     return htmlspecialchars($html);
